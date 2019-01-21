955c11c941aababe767f9b06d5c67a2d1944f2d9
wangkeshan9538 <wangkeshan9538@gmail.com>
2019-01-21 23:00:06 +0800

    temp

---
+++ src/test/java/com/wks/netty_test/FirstClientHandler.java
@@ -0,0 +1,34 @@
+package com.wks.netty_test;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+public class FirstClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println(new Date() + ": 客户端写出数据");
+
+        // 1. 获取数据
+        ByteBuf buffer = getByteBuf(ctx);
+
+        // 2. 写数据
+        ctx.channel().writeAndFlush(buffer);
+    }
+
+    private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+        // 1. 获取二进制抽象 ByteBuf
+        ByteBuf buffer = ctx.alloc().buffer();
+
+        // 2. 准备数据，指定字符串的字符集为 utf-8
+        byte[] bytes = "你好，闪电侠!".getBytes(Charset.forName("utf-8"));
+
+        // 3. 填充数据到 ByteBuf
+        buffer.writeBytes(bytes);
+
+        return buffer;
+    }
+}
--- src/test/java/com/wks/netty_test/NettyClient.java
+++ src/test/java/com/wks/netty_test/NettyClient.java
@@ -3,11 +3,14 @@ package com.wks.netty_test;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.AttributeKey;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class NettyClient {
     public static void main(String[] args) throws InterruptedException {
@@ -15,11 +18,15 @@ public class NettyClient {
         NioEventLoopGroup group = new NioEventLoopGroup();
 
         bootstrap.group(group)
+                .attr(AttributeKey.newInstance("clientName"), "nettyClient")
                 .channel(NioSocketChannel.class)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true)
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) {
-                        ch.pipeline().addLast(new StringEncoder());
+                        ch.pipeline().addLast(new FirstClientHandler());
                     }
                 });
 
@@ -32,14 +39,34 @@ public class NettyClient {
 
         }).channel();
 
-        //while (true) {
-            channel.writeAndFlush(new Date() + ": hello world!");
-            Thread.sleep(2000);
-        //}
-        channel.close();
-
+        connect(bootstrap, "localhost", 8080, MAX_RETRY);
     }
+
+    final static int MAX_RETRY=5;
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.out.println(delay+"sss");
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit
+                        .SECONDS);
+            }
+        });
+    }
+
 }
 
 
 
+/* 调 workerGroup 的 schedule 方法即可实现定时任务逻辑。  并不是循环执行 ，只执行一次 */
+
+/* ch.pipeline() 返回的是和这条连接相关的逻辑处理链，采用了责任链模式*/
